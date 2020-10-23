be9d5e7991141e9f521d37bcbdbc09b9d6da3203
DevExpressExampleBot <fakeEmail>
2020-10-23 17:12:07 +0300

    Source auto update [skip ci]

--- VB/ConditionalFormatting/MainWindow.xaml.vb
+++ VB/ConditionalFormatting/MainWindow.xaml.vb
@@ -2,32 +2,45 @@
 Imports DevExpress.Xpf.Core.ConditionalFormatting
 Imports System.Windows
 Imports System.Windows.Media
+Imports System.Collections.Generic
 
 Namespace ConditionalFormatting
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-            Dim salesVsTargetFormatCondition = New FormatCondition() With {.Expression = "[SalesVsTarget] < 0.0m", .FieldName = "SalesVsTarget", .PredefinedFormatName = "RedText"}
-            view.FormatConditions.Add(salesVsTargetFormatCondition)
-
-            Dim profitFormatCondition = New FormatCondition() With { _
-                .Expression = "[Profit] < 0.0", .FieldName = "Profit", .Format = New Format() With {.Foreground = Brushes.Red} _
-            }
-            view.FormatConditions.Add(profitFormatCondition)
-
-            Dim salesDataBar = New DataBarFormatCondition() With {.FieldName = "Sales", .PredefinedFormatName = "RedGradientDataBar"}
-            view.FormatConditions.Add(salesDataBar)
-
-            Dim _topBottomRule = New TopBottomRuleFormatCondition() With {.Expression = "[Sales]", .FieldName = Nothing, .PredefinedFormatName = "BoldText", .Rule = TopBottomRule.TopPercent, .Threshold = 10.0R}
-            view.FormatConditions.Add(_topBottomRule)
-
-            Dim profitDataBar = New DataBarFormatCondition() With {.FieldName = "Profit", .PredefinedFormatName = "GreenGradientDataBar"}
-            view.FormatConditions.Add(profitDataBar)
-
-            Dim marketShareIcons = New IconSetFormatCondition() With {.FieldName = "MarketShare", .PredefinedFormatName = "Quarters5IconSet"}
-            view.FormatConditions.Add(marketShareIcons)
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+			view.FormatConditions.AddRange(New List(Of FormatConditionBase) From {
+				New FormatCondition() With {
+					.Expression = "[SalesVsTarget] < 0.0m",
+					.FieldName = "SalesVsTarget",
+					.PredefinedFormatName = "RedText"
+				},
+				New FormatCondition() With {
+					.Expression = "[Profit] < 0.0",
+					.FieldName = "Profit",
+					.Format = New Format() With {.Foreground = Brushes.Red}
+				},
+				New DataBarFormatCondition() With {
+					.FieldName = "Sales",
+					.PredefinedFormatName = "RedGradientDataBar"
+				},
+				New TopBottomRuleFormatCondition() With {
+					.Expression = "[Sales]",
+					.FieldName = Nothing,
+					.PredefinedFormatName = "BoldText",
+					.Rule = TopBottomRule.TopPercent,
+					.Threshold = 10R
+				},
+				New DataBarFormatCondition() With {
+					.FieldName = "Profit",
+					.PredefinedFormatName = "GreenGradientDataBar"
+				},
+				New IconSetFormatCondition() With {
+					.FieldName = "MarketShare",
+					.PredefinedFormatName = "Quarters5IconSet"
+				}
+			})
+		End Sub
+	End Class
 End Namespace
