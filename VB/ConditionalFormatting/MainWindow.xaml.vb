b1643e9d366289aa5586e5ac09d8d30ab0c22578
DevExpressExampleBot <fakeEmail>
2026-02-03 23:24:11 +0400

    Source auto update [skip ci]

--- VB/ConditionalFormatting/MainWindow.xaml.vb
+++ VB/ConditionalFormatting/MainWindow.xaml.vb
@@ -1,33 +1,27 @@
-﻿Imports DevExpress.Xpf.Grid
+Imports DevExpress.Xpf.Grid
 Imports DevExpress.Xpf.Core.ConditionalFormatting
 Imports System.Windows
 Imports System.Windows.Media
 
 Namespace ConditionalFormatting
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
             Dim salesVsTargetFormatCondition = New FormatCondition() With {.Expression = "[SalesVsTarget] < 0.0m", .FieldName = "SalesVsTarget", .PredefinedFormatName = "RedText"}
-            view.FormatConditions.Add(salesVsTargetFormatCondition)
-
-            Dim profitFormatCondition = New FormatCondition() With { _
-                .Expression = "[Profit] < 0.0", .FieldName = "Profit", .Format = New Format() With {.Foreground = Brushes.Red} _
-            }
-            view.FormatConditions.Add(profitFormatCondition)
-
+            Me.view.FormatConditions.Add(salesVsTargetFormatCondition)
+            Dim profitFormatCondition = New FormatCondition() With {.Expression = "[Profit] < 0.0", .FieldName = "Profit", .Format = New Format() With {.Foreground = Brushes.Red}}
+            Me.view.FormatConditions.Add(profitFormatCondition)
             Dim salesDataBar = New DataBarFormatCondition() With {.FieldName = "Sales", .PredefinedFormatName = "RedGradientDataBar"}
-            view.FormatConditions.Add(salesDataBar)
-
-            Dim _topBottomRule = New TopBottomRuleFormatCondition() With {.Expression = "[Sales]", .FieldName = Nothing, .PredefinedFormatName = "BoldText", .Rule = TopBottomRule.TopPercent, .Threshold = 10.0R}
-            view.FormatConditions.Add(_topBottomRule)
-
+            Me.view.FormatConditions.Add(salesDataBar)
+            Dim topBottomRule = New TopBottomRuleFormatCondition() With {.Expression = "[Sales]", .FieldName = Nothing, .PredefinedFormatName = "BoldText", .Rule = DevExpress.Xpf.Core.ConditionalFormatting.TopBottomRule.TopPercent, .Threshold = 10R}
+            Me.view.FormatConditions.Add(topBottomRule)
             Dim profitDataBar = New DataBarFormatCondition() With {.FieldName = "Profit", .PredefinedFormatName = "GreenGradientDataBar"}
-            view.FormatConditions.Add(profitDataBar)
-
+            Me.view.FormatConditions.Add(profitDataBar)
             Dim marketShareIcons = New IconSetFormatCondition() With {.FieldName = "MarketShare", .PredefinedFormatName = "Quarters5IconSet"}
-            view.FormatConditions.Add(marketShareIcons)
+            Me.view.FormatConditions.Add(marketShareIcons)
         End Sub
     End Class
 End Namespace
