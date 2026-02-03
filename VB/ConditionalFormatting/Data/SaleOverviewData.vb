2e2c07265c066f9d778caff7b0264797059a10ed
DevExpressExampleBot <fakeEmail>
2026-02-03 23:28:24 +0400

    Source auto update [skip ci]

--- VB/ConditionalFormatting/Data/SaleOverviewData.vb
+++ VB/ConditionalFormatting/Data/SaleOverviewData.vb
@@ -1,31 +1,33 @@
-﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations
 
 Namespace ConditionalFormatting
-	Public Class SaleOverviewData
-		Public Sub New(ByVal state As String, ByVal sales As Double, ByVal salesVsTarget As Double, ByVal profit As Double, ByVal customersSatisfaction As Double, ByVal markerShare As Double)
-			Me.State = state
-			Me.Sales = sales
-			Me.Profit = profit
-			Me.SalesVsTarget = salesVsTarget
-			Me.CustomersSatisfaction = customersSatisfaction
-			Me.MarketShare = markerShare
-		End Sub
-		Public Property State() As String
 
-		<DisplayFormat(DataFormatString := "#,##0,,M")>
-		Public Property Sales() As Double
+    Public Class SaleOverviewData
 
-		<DisplayFormat(DataFormatString := "#,##0,,M")>
-		Public Property Profit() As Double
+        Public Sub New(ByVal state As String, ByVal sales As Double, ByVal salesVsTarget As Double, ByVal profit As Double, ByVal customersSatisfaction As Double, ByVal markerShare As Double)
+            Me.State = state
+            Me.Sales = sales
+            Me.Profit = profit
+            Me.SalesVsTarget = salesVsTarget
+            Me.CustomersSatisfaction = customersSatisfaction
+            MarketShare = markerShare
+        End Sub
 
-		<DisplayFormat(DataFormatString := "p", ApplyFormatInEditMode := True), Display(Name := "Sales vs Target")>
-		Public Property SalesVsTarget() As Double
+        Public Property State As String
 
-		<DisplayFormat(DataFormatString := "p0", ApplyFormatInEditMode := True)>
-		Public Property MarketShare() As Double
+        <DisplayFormat(DataFormatString:="#,##0,,M")>
+        Public Property Sales As Double
 
-		<Display(Name := "Cust Satisfaction")>
-		Public Property CustomersSatisfaction() As Double
+        <DisplayFormat(DataFormatString:="#,##0,,M")>
+        Public Property Profit As Double
 
-	End Class
+        <DisplayFormat(DataFormatString:="p", ApplyFormatInEditMode:=True), Display(Name:="Sales vs Target")>
+        Public Property SalesVsTarget As Double
+
+        <DisplayFormat(DataFormatString:="p0", ApplyFormatInEditMode:=True)>
+        Public Property MarketShare As Double
+
+        <Display(Name:="Cust Satisfaction")>
+        Public Property CustomersSatisfaction As Double
+    End Class
 End Namespace
